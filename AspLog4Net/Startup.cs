0a7b9a5b69e430b356d6ce02bb81d016a880c814
windcatcher <lms681@163.com>
2018-01-14 19:17:12 +0800

    增加 AspLog4Net项目，支持webapi 的log输出

---
+++ AspLog4Net/Startup.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using log4net;
+using log4net.Config;
+using log4net.Repository;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace AspLog4Net
+{
+    public class Startup
+    {
+        public static ILoggerRepository repository { get; set; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            repository = LogManager.CreateRepository("NETCoreRepository");
+            XmlConfigurator.Configure(repository, new FileInfo("log4net.config"));
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+            var log = LogManager.GetLogger(repository.Name, typeof(Startup));
+            log.Info("test");
+        }
+    }
+}
