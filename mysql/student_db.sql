4db408a0eb714960190715b9ef30b720b37996f3
windcatcher <lms681@163.com>
2018-01-14 15:47:21 +0800

    增加sql文件

---
+++ mysql/student_db.sql
@@ -0,0 +1,35 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.1.73-log : Database - student_db
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`student_db` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `student_db`;
+
+/*Table structure for table `student_tb` */
+
+DROP TABLE IF EXISTS `student_tb`;
+
+CREATE TABLE `student_tb` (
+  `id` int(3) DEFAULT NULL,
+  `name` char(10) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/*Data for the table `student_tb` */
+
+insert  into `student_tb`(`id`,`name`) values (3,'student10');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
