cfd43778f969004104556baab60938d7d61535b7
windcatcher <lms681@163.com>
2018-01-14 16:51:29 +0800

    添加控制台的log4net

--- log4netForTest/Program.cs
+++ log4netForTest/Program.cs
@@ -1,21 +1,38 @@
 ﻿using System;
+using System.IO;
 using log4net;
+using log4net.Config;
+using log4net.Repository;
 
+namespace log4netForTest {
+    class Program {
+        static void Main (string[] args) {
+            //  RunLogOnConsole ();
+            LogForFile();
+        }
 
-namespace log4netForTest
-{
-    class Program
-    {
-        static void Main(string[] args)
+        static void RunLogOnConsole () {
+            ILoggerRepository repository = LogManager.CreateRepository ("NETCoreRepository");
+            // 默认简单配置，输出至控制台
+            BasicConfigurator.Configure (repository);
+            ILog log = LogManager.GetLogger (repository.Name, "NETCorelog4net");
+
+            log.Info ("NETCorelog4net log");
+            log.Info ("test log");
+            log.Error ("error");
+            Console.ReadKey ();
+        }
+
+        static void LogForFile()
         {
             ILoggerRepository repository = LogManager.CreateRepository("NETCoreRepository");
-            // 默认简单配置，输出至控制台
-            BasicConfigurator.Configure(repository);
-            ILog log = LogManager.GetLogger(repository.Name,"NETCorelog4net");
+            XmlConfigurator.Configure(repository, new FileInfo("log4net.config"));
+            ILog log = LogManager.GetLogger(repository.Name, "NETCorelog4net");
 
             log.Info("NETCorelog4net log");
             log.Info("test log");
             log.Error("error");
+            log.Info("linezero");
             Console.ReadKey();
         }
     }
