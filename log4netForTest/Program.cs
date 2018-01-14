df29431b715e7f9a93b599250b69160083021285
windcatcher <lms681@163.com>
2018-01-14 16:28:44 +0800

    log4net 项目

---
+++ log4netForTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using log4net;
+
+
+namespace log4netForTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILoggerRepository repository = LogManager.CreateRepository("NETCoreRepository");
+            // 默认简单配置，输出至控制台
+            BasicConfigurator.Configure(repository);
+            ILog log = LogManager.GetLogger(repository.Name,"NETCorelog4net");
+
+            log.Info("NETCorelog4net log");
+            log.Info("test log");
+            log.Error("error");
+            Console.ReadKey();
+        }
+    }
+}
