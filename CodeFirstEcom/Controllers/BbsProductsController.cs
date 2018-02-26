edea123162c642ea7f8fdc8254bf95281a267575
windcatcher <lms681@163.com>
2018-02-26 13:38:24 +0800

    提交更新

---
+++ CodeFirstEcom/Controllers/BbsProductsController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CodeFirstEcom;
+
+namespace CodeFirstEcom.Controllers
+{
+    public class BbsProductsController : Controller
+    {
+        private readonly ecomContext _context;
+
+        public BbsProductsController(ecomContext context)
+        {
+            _context = context;
+        }
+
+        // GET: BbsProducts
+        public async Task<IActionResult> Index()
+        {
+            var ecomContext = _context.BbsProduct.Include(b => b.Brand).Include(b => b.Type);
+            return View(await ecomContext.ToListAsync());
+        }
+
+        // GET: BbsProducts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bbsProduct = await _context.BbsProduct
+                .Include(b => b.Brand)
+                .Include(b => b.Type)
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (bbsProduct == null)
+            {
+                return NotFound();
+            }
+
+            return View(bbsProduct);
+        }
+
+        // GET: BbsProducts/Create
+        public IActionResult Create()
+        {
+            ViewData["BrandId"] = new SelectList(_context.BbsBrand, "Id", "Id");
+            ViewData["TypeId"] = new SelectList(_context.BbsType, "Id", "Id");
+            return View();
+        }
+
+        // POST: BbsProducts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,No,Name,Weight,IsNew,IsHot,IsCommend,CreateTime,CreateUserId,CheckTime,CheckUserId,IsShow,IsDel,TypeId,BrandId,Keywords,Sales,Description,PackageList,Feature,Color,Size")] BbsProduct bbsProduct)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(bbsProduct);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BrandId"] = new SelectList(_context.BbsBrand, "Id", "Id", bbsProduct.BrandId);
+            ViewData["TypeId"] = new SelectList(_context.BbsType, "Id", "Id", bbsProduct.TypeId);
+            return View(bbsProduct);
+        }
+
+        // GET: BbsProducts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bbsProduct = await _context.BbsProduct.SingleOrDefaultAsync(m => m.Id == id);
+            if (bbsProduct == null)
+            {
+                return NotFound();
+            }
+            ViewData["BrandId"] = new SelectList(_context.BbsBrand, "Id", "Id", bbsProduct.BrandId);
+            ViewData["TypeId"] = new SelectList(_context.BbsType, "Id", "Id", bbsProduct.TypeId);
+            return View(bbsProduct);
+        }
+
+        // POST: BbsProducts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,No,Name,Weight,IsNew,IsHot,IsCommend,CreateTime,CreateUserId,CheckTime,CheckUserId,IsShow,IsDel,TypeId,BrandId,Keywords,Sales,Description,PackageList,Feature,Color,Size")] BbsProduct bbsProduct)
+        {
+            if (id != bbsProduct.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(bbsProduct);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BbsProductExists(bbsProduct.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BrandId"] = new SelectList(_context.BbsBrand, "Id", "Id", bbsProduct.BrandId);
+            ViewData["TypeId"] = new SelectList(_context.BbsType, "Id", "Id", bbsProduct.TypeId);
+            return View(bbsProduct);
+        }
+
+        // GET: BbsProducts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bbsProduct = await _context.BbsProduct
+                .Include(b => b.Brand)
+                .Include(b => b.Type)
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (bbsProduct == null)
+            {
+                return NotFound();
+            }
+
+            return View(bbsProduct);
+        }
+
+        // POST: BbsProducts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var bbsProduct = await _context.BbsProduct.SingleOrDefaultAsync(m => m.Id == id);
+            _context.BbsProduct.Remove(bbsProduct);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BbsProductExists(int id)
+        {
+            return _context.BbsProduct.Any(e => e.Id == id);
+        }
+    }
+}
--- CodeFirstEcom/Entity/ecomContextSeed.cs
+++ CodeFirstEcom/Entity/ecomContextSeed.cs
@@ -10,38 +10,6 @@ using System.Threading.Tasks;
 
 namespace CodeFirstEcom.Entity
 {
-    public class EcomContextSeed
-    {
-        public async Task SeedAsync(ecomContext context)
-        {
-            if (!context.BbsBuyer.Any())
-            {
-                context.BbsBuyer.AddRange(GetPreconfiguredBbsBuyers());
-                await context.SaveChangesAsync();
-            }
-            //if (!context.BbsBrand.Any())
-            //{
-            //    context.BbsBrand.AddRange(GetPreconfiguredBbsBrands());
-            //    await context.SaveChangesAsync();
-            //}
-        }
-
-        private IEnumerable<BbsBuyer> GetPreconfiguredBbsBuyers()
-        {
-            return new List<BbsBuyer>()
-            {
-                new BbsBuyer(){Username="fbb2014",Password="e10adc3949ba59abbe56e057f20f883e",Email="112624349@qq.com",Gender="WOMAN",RealName="范冰冰",Province="120000",City="120100",Town="120105",Addr="海淀区建材城西路100号",IsDel=1},
-            };
-        }
-        //private IEnumerable<BbsBrand> GetPreconfiguredBbsBrands()
-        //{
-        //    return new List<BbsBrand>()
-        //    {
-        //        new BbsBrand(){Username="fbb2014",Password="e10adc3949ba59abbe56e057f20f883e",Email="112624349@qq.com",Gender="WOMAN",RealName="范冰冰",Province="120000",City="120100",Town="120105",Addr="海淀区建材城西路100号",IsDel=1},
-        //    };
-        //}
-    }
-
     public static class IWebHostExtensions
     {
         public static IWebHost MigrateDbContext<TContext>(this IWebHost webHost, Action<TContext, IServiceProvider> seeder) where TContext : DbContext
@@ -85,6 +53,44 @@ namespace CodeFirstEcom.Entity
             }
 
             return webHost;
+        }
+    }
+
+    public class EcomContextSeed
+    {
+        public async Task SeedAsync(ecomContext context)
+        {
+            if (!context.BbsBuyer.Any())
+            {
+                context.BbsBuyer.AddRange(GetPreconfiguredBbsBuyers());
+                await context.SaveChangesAsync();
+            }
+            if (!context.BbsBrand.Any())
+            {
+                context.BbsBrand.AddRange(GetPreconfiguredBbsBrands());
+                await context.SaveChangesAsync();
+            }
+        }
+
+        private IEnumerable<BbsBrand> GetPreconfiguredBbsBrands()
+        {
+            return new List<BbsBrand>()
+            {
+                new BbsBrand(){Name="依琦莲",ImgUrl="upload/20141201101326051272.jpg",Sort=99,IsDisplay=1},
+                new BbsBrand(){Name="凯速",IsDisplay=1},
+                new BbsBrand(){Name="伊朵莲",IsDisplay=1},
+                new BbsBrand(){Name="菩媞",IsDisplay=1},
+                new BbsBrand(){Name="丹璐斯",IsDisplay=1},
+                new BbsBrand(){Name="喜悦瑜伽",IsDisplay=1},
+            };
+        }
+
+        private IEnumerable<BbsBuyer> GetPreconfiguredBbsBuyers()
+        {
+            return new List<BbsBuyer>()
+            {
+                new BbsBuyer(){Username="fbb2014",Password="e10adc3949ba59abbe56e057f20f883e",Email="112624349@qq.com",Gender="WOMAN",RealName="范冰冰",Province="120000",City="120100",Town="120105",Addr="海淀区建材城西路100号",IsDel=1},
+            };
         }
     }
 }
