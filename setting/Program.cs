b4c36a8958c578b998ce6fb081541370e6e3be1c
windcatcher <lms681@163.com>
2018-02-27 14:44:19 +0800

    增加配置测试项目

---
+++ setting/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+namespace setting
+{
+    class Program
+    {
+        public static IConfiguration Configuration { set; get; }
+
+        static void Main(string[] args)
+        {
+            InitSetting();
+        }
+
+
+        static void InitSetting()
+        {
+            var builder = new ConfigurationBuilder();
+
+            builder.AddJsonFile("setting.json");
+            Configuration = builder.Build();
+            Console.WriteLine($"option1={Configuration["option1"]}");
+            Console.WriteLine($"option2={Configuration["option2"]}");
+            Console.WriteLine("students:");
+            Console.Write($"{Configuration["student:0:name"]}, ");
+            Console.Write($"{Configuration["student:0:age"]}, ");
+            Console.Write($"{Configuration["student:1:name"]}, ");
+            Console.Write($"{Configuration["student:1:age"]}, ");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ setting/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace setting
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+
+        }
+
+    }
+}
