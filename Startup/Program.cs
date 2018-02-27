774165f7de58b409049234d6c7bfccbd4221806f
windcatcher <lms681@163.com>
2018-02-27 14:45:04 +0800

    asp启动项

---
+++ Startup/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Startup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
