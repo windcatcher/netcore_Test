821cac453e1fe49c28bccdc56a6e933d5d0ce8c0
windcatcher <lms681@163.com>
2018-03-11 03:13:01 +0800

    使用Hybrid Flow并添加API访问控制

---
+++ IdentityServer4/HybridFlowAuthenticationWithApiAccess/IdentityServer/Config.cs
@@ -0,0 +1,101 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace IdentityServer
+{
+    public class Config
+    {
+        //增加oidc的identity的scope
+        public static IEnumerable<IdentityResource> GetIdentityResource()
+        {
+            return new List<IdentityResource> {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+        }
+        public static IEnumerable<Client> GetApiClients()
+        {
+            return new List<Client> {
+                new Client(){
+                    ClientId="client",
+                     // 没有交互性用户，使用 clientid/secret 实现认证。
+                    AllowedGrantTypes=GrantTypes.ClientCredentials,
+                    //用于认证的密码
+                    ClientSecrets={new Secret("secret".Sha256())},
+                    //客户端有权访问的范围
+                    AllowedScopes ={ "api1"}
+                },
+                //密码认证模式
+                 new Client(){
+                    ClientId="ro.client",
+                    AllowedGrantTypes=GrantTypes.ResourceOwnerPassword,
+                    //用于认证的密码
+                    ClientSecrets={new Secret("secret".Sha256())},
+                    //客户端有权访问的范围
+                    AllowedScopes ={ "api1"}
+                },
+                 new Client()
+                 {
+                     //客户端允许服务器到服务器API调用
+                     ClientId="mvc",
+                     ClientName="MVC Client",
+                     AllowedGrantTypes=GrantTypes.HybridAndClientCredentials,
+                      //用于认证的密码
+                     ClientSecrets={new Secret("secret".Sha256())},
+                     RequireConsent=true, //默认为ture
+                     //登录后重定向到客户端
+                     RedirectUris={"http://localhost:5002/signin-oidc" },
+                     //注销后重定向到客户端
+                     PostLogoutRedirectUris={"http://localhost:5002/signout-callback-oidc" },
+                     AllowedScopes=new List<string>{
+                         IdentityServerConstants.StandardScopes.OpenId,
+                         IdentityServerConstants.StandardScopes.Profile,
+                           "api1"
+                     },
+                       AllowOfflineAccess = true //允许为长时间的API访问请求刷新令牌
+                 }
+                };
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource ("api1","my api") };
+        }
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser> {
+                new TestUser{
+                SubjectId ="1",
+                Username="alice",
+                Password="alice",
+                Claims = new List<Claim>
+                 {
+                        new Claim("name", "Alice"),
+                        new Claim("website", "https://alice.com")
+                    }
+                },
+                new TestUser{
+                SubjectId ="2",
+                Username="bob",
+                Password="bob",
+                  Claims = new List<Claim>
+                    {
+                        new Claim("name", "Bob"),
+                        new Claim("website", "https://bob.com")
+                    }
+                },
+            };
+        }
+
+
+
+    }
+}
---
+++ IdentityServer4/HybridFlowAuthenticationWithApiAccess/IdentityServer/Startup.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace IdentityServer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            //使用内存存储，密钥，客户端和资源来配置身份服务器
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddInMemoryIdentityResources(Config.GetIdentityResource())
+                .AddInMemoryApiResources(Config.GetApiResources())
+                .AddInMemoryClients(Config.GetApiClients())
+                .AddTestUsers(Config.GetUsers());
+
+            services.AddAuthentication()
+               .AddGoogle("Google", options =>
+               {
+                   options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+                   //options.ClientId = "your ClientId";
+                   //options.ClientSecret = "your ClientSecret";
+                   options.ClientId = "434483408261-55tc8n0cs4ff1fe21ea8df2o443v2iuc.apps.googleusercontent.com";
+                   options.ClientSecret = "3gcoTrEDPPJ0ukn_aYYT6PWo";
+               })
+              .AddMicrosoftAccount(microsoftOptions =>
+               {
+                   microsoftOptions.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+                   microsoftOptions.ClientId = "your ClientId";
+                   microsoftOptions.ClientSecret = "your ClientSecret";
+               })
+               .AddOpenIdConnect("oidc", "OpenID Connect", options =>
+               {
+                   options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+                   options.SignOutScheme = IdentityServerConstants.SignoutScheme;
+
+                   options.Authority = "https://demo.identityserver.io/";
+                   options.ClientId = "implicit";
+
+                   options.TokenValidationParameters = new TokenValidationParameters
+                   {
+                       NameClaimType = "name",
+                       RoleClaimType = "role"
+                   };
+               });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseIdentityServer();
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
