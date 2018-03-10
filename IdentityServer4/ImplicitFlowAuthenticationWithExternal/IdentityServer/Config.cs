60901842f9c61c00a0c1f2d9d6d4460feae39e0f
windcatcher <lms681@163.com>
2018-03-11 00:31:24 +0800

    增加identityserver4的auth2.0+oidc 的简化模式

---
+++ IdentityServer4/ImplicitFlowAuthenticationWithExternal/IdentityServer/Config.cs
@@ -0,0 +1,85 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer
+{
+    public class Config
+    {
+        //增加oidc的identity的scope
+        public static IEnumerable<IdentityResource> GetIdentityResource()
+        {
+            return new List<IdentityResource> {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+        }
+        public static IEnumerable<Client> GetApiClients()
+        {
+            return new List<Client> {
+                new Client(){
+                    ClientId="client",
+                     // 没有交互性用户，使用 clientid/secret 实现认证。
+                    AllowedGrantTypes=GrantTypes.ClientCredentials,
+                    //用于认证的密码
+                    ClientSecrets={new Secret("secret".Sha256())},
+                    //客户端有权访问的范围
+                    AllowedScopes ={ "api1"}
+                },
+                //密码认证模式
+                 new Client(){
+                    ClientId="ro.client",
+                    AllowedGrantTypes=GrantTypes.ResourceOwnerPassword,
+                    //用于认证的密码
+                    ClientSecrets={new Secret("secret".Sha256())},
+                    //客户端有权访问的范围
+                    AllowedScopes ={ "api1"}
+                },
+                 new Client()
+                 {
+                     ClientId="mvc",
+                     ClientName="MVC Client",
+                     AllowedGrantTypes=GrantTypes.Implicit,
+                     RequireConsent=true, //默认为ture
+                     //登录后重定向到客户端
+                     RedirectUris={"http://localhost:5002/signin-oidc" },
+                     //注销后重定向到客户端
+                     PostLogoutRedirectUris={"http://localhost:5002/signout-callback-oidc" },
+                     AllowedScopes=new List<string>{
+                         IdentityServerConstants.StandardScopes.OpenId,
+                         IdentityServerConstants.StandardScopes.Profile
+                     },
+                 }
+                };
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource ("api1","my api") };
+        }
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser> {
+                new TestUser{
+                SubjectId ="1",
+                Username="alice",
+                Password="password"
+                },
+                new TestUser{
+                SubjectId ="2",
+                Username="bob",
+                Password="password"
+                },
+            };
+        }
+
+
+
+    }
+}
---
+++ IdentityServer4/ImplicitFlowAuthenticationWithExternal/MvcClient/Startup.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MvcClient
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+            //AddAuthentication将认证服务注入到容器中，使用cookie作为认证用户的主要方法
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = "Cookies";
+                options.DefaultChallengeScheme = "oidc";//用户登录时，使用openID connection方案
+            })
+            .AddCookie("Cookies") //添加可以处理cookie的处理程序
+            .AddOpenIdConnect("oidc", options => //用户配置执行openID connect协议的处理程序
+            {
+                options.SignInScheme = "Cookies";//用于oidc协议完成后使用cookie处理程序发出cookie
+                options.Authority = "http://localhost:5000";
+                options.RequireHttpsMetadata = false;
+                options.ClientId = "mvc";
+                options.SaveTokens = true;//用于在cookie中保存identityserver的token令牌
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
