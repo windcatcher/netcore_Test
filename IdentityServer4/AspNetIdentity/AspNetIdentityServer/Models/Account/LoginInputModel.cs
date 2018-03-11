10476112d4c5b9eb00a4a838671d5134fe0c116f
windcatcher <lms681@163.com>
2018-03-11 18:58:01 +0800

    ASP.NET Core Identity 和 IdentityServer4集成

---
+++ IdentityServer4/AspNetIdentity/AspNetIdentityServer/Models/Account/LoginInputModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Models
+{
+    public class LoginInputModel
+    {
+        [Required]
+        public string Username { get; set; }
+        public string Password { get; set; }
+        [Required]
+        public bool RememberLogin { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
---
+++ IdentityServer4/AspNetIdentity/AspNetIdentityServer/Startup.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using AspNetIdentityServer.Data;
+using AspNetIdentityServer.Models;
+using AspNetIdentityServer.Services;
+using IdentityServer;
+using IdentityServer4;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AspNetIdentityServer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            // Add application services.
+            services.AddTransient<IEmailSender, EmailSender>();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings
+                options.Password.RequireDigit = true;
+                //options.Password.RequiredLength = 8;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+                //options.Password.RequiredUniqueChars = 6;
+            });
+            services.AddMvc();
+
+            // configure identity server with in-memory stores, keys, clients and scopes
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddInMemoryPersistedGrants()
+                .AddInMemoryIdentityResources(Config.GetIdentityResource())
+                .AddInMemoryApiResources(Config.GetApiResources())
+                .AddInMemoryClients(Config.GetApiClients())
+                .AddAspNetIdentity<ApplicationUser>();
+
+            services.AddAuthentication()
+              .AddGoogle("Google", options =>
+              {
+                  options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+                   //options.ClientId = "your ClientId";
+                   //options.ClientSecret = "your ClientSecret";
+                   options.ClientId = "434483408261-55tc8n0cs4ff1fe21ea8df2o443v2iuc.apps.googleusercontent.com";
+                  options.ClientSecret = "3gcoTrEDPPJ0ukn_aYYT6PWo";
+              })
+             .AddMicrosoftAccount(microsoftOptions =>
+             {
+                 microsoftOptions.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+                 microsoftOptions.ClientId = "your ClientId";
+                 microsoftOptions.ClientSecret = "your ClientSecret";
+             })
+              .AddOpenIdConnect("oidc", "OpenID Connect", options =>
+              {
+                  options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+                  options.SignOutScheme = IdentityServerConstants.SignoutScheme;
+
+                  options.Authority = "https://demo.identityserver.io/";
+                  options.ClientId = "implicit";
+
+                  options.TokenValidationParameters = new TokenValidationParameters
+                  {
+                      NameClaimType = "name",
+                      RoleClaimType = "role"
+                  };
+              });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            //app.UseAuthentication();
+            app.UseIdentityServer();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
