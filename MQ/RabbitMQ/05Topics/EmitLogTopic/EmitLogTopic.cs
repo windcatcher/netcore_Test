bdc7c084253f479f140c5763af5ba21f222a39db
windcatcher <lms681@163.com>
2018-03-13 02:47:38 +0800

    使用 主题交换机

---
+++ MQ/RabbitMQ/05Topics/EmitLogTopic/EmitLogTopic.cs
@@ -0,0 +1,51 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace EmitLogTopic
+{
+    class Program
+    {
+        private const string IP_ADDRESS = "127.0.0.1";
+        private const int PORT = 5672;//服务端默认端口
+        private const string USER_NAME = "guest";
+        private const string PASSWORD = "guest";
+        static void Main(string[] args)
+        {
+            //1.创建factory
+            var factory = new ConnectionFactory
+            {
+                HostName = IP_ADDRESS,
+                Port = PORT,
+                UserName = USER_NAME,
+                Password = PASSWORD
+            };
+            //2.创建connection
+            using (var con = factory.CreateConnection())
+            {
+                //创建channel
+                using (var channel = con.CreateModel())
+                {
+                    //创建topic类型的主题交换机
+                    channel.ExchangeDeclare(exchange: "topic_logs", type: "topic");
+
+                    var routingKey = (args.Length > 0) ? args[0] : "anonymous.info";
+
+                    var msg = (args.Length > 1)
+                         ? string.Join(" ", args.Skip(1).ToArray())
+                         : "Hello World!";
+                    var body = Encoding.UTF8.GetBytes(msg);
+
+                    //发布消息,使用主题交换机，根据主题的匹配键使用不同的绑定，使用默认的临时队列
+                    channel.BasicPublish(exchange: "topic_logs", routingKey: routingKey, basicProperties: null, body: body);
+                    Console.WriteLine(" [x] Sent '{0}':'{1}'", routingKey, msg);
+                }
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ MQ/RabbitMQ/05Topics/ReceiveLogsTopic/ReceiveLogsTopic.cs
@@ -0,0 +1,78 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace ReceiveLogsTopic
+{
+    class Program
+    {
+        private const string IP_ADDRESS = "127.0.0.1";
+        private const int PORT = 5672;//服务端默认端口
+        private const string USER_NAME = "guest";
+        private const string PASSWORD = "guest";
+        static void Main(string[] args)
+        {
+            //1.创建factory
+            var factory = new ConnectionFactory
+            {
+                HostName = IP_ADDRESS,
+                Port = PORT,
+                UserName = USER_NAME,
+                Password = PASSWORD
+            };
+            //2.创建connection
+            using (var con = factory.CreateConnection())
+            {
+                //创建channel
+                using (var channel = con.CreateModel())
+                {
+                    //创建topic类型的主题交换机
+                    channel.ExchangeDeclare(exchange: "topic_logs",
+                                     type: "topic");
+                    //使用默认的临时队列
+                    var queueName = channel.QueueDeclare().QueueName;
+
+
+                    if (args.Length < 1)
+                    {
+                        Console.Error.WriteLine("Usage: {0} [binding_key...]",
+                                         Environment.GetCommandLineArgs()[0]);
+                        Console.WriteLine(" Press [enter] to exit.");
+                        Console.ReadLine();
+                        Environment.ExitCode = 1;
+                        return;
+                    }
+
+                    //根据主题的匹配键使用不同的绑定
+                    foreach (var bindingKey in args)
+                    {
+                        channel.QueueBind(queue: queueName,
+                                          exchange: "topic_logs",
+                                          routingKey: bindingKey);
+                    }
+
+                    Console.WriteLine(" [*] Waiting for messages. To exit press CTRL+C");
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        var routingKey = ea.RoutingKey;
+                        Console.WriteLine(" [x] Received '{0}':'{1}'",
+                                          routingKey, message);
+                    };
+                    channel.BasicConsume(queue: queueName,
+                                         autoAck: true,
+                                         consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+
+        }
+    }
+}
