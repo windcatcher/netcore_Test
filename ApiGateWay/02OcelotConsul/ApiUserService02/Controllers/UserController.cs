df72df5b325c8796ce2a643405275ad53e9188b7
windcatcher <lms681@163.com>
2018-04-07 13:25:31 +0800

    增加userapi02模拟负载均衡

---
+++ ApiGateWay/02OcelotConsul/ApiUserService02/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiUserService.Controllers
+{
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        // GET api/User
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "User102", "User202" };
+        }
+
+        // GET api/User/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/User
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/User/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/User/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
