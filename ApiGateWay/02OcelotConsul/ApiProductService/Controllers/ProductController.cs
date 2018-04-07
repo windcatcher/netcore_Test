5ce1f8bed52f336324c5912177c651db215f561d
windcatcher <lms681@163.com>
2018-04-07 12:50:02 +0800

    增加cosul与ocelot结合：服务发现与健康检查

---
+++ ApiGateWay/02OcelotConsul/ApiProductService/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProductService.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductController : Controller
+    {
+        // GET api/Product
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Product1", "Product2" };
+        }
+
+        // GET api/Product/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/Product
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/Product/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/Product/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
