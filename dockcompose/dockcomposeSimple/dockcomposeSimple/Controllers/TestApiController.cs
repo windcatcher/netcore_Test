d5d6cb5a892fad2cd6ddd6fae280a91c3c5c0100
windcatcher <lms681@163.com>
2018-04-09 23:48:21 +0800

    增加 dockcomposeSimple 测试 两个容器间web 互相访问的网络ip

---
+++ dockcompose/dockcomposeSimple/dockcomposeSimple/Controllers/TestApiController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace dockcomposeSimple.Controllers
+{
+    public class TestApiController : Controller
+    {
+        private readonly IOptionsSnapshot<AppSettings> _settings;
+
+        public TestApiController(IOptionsSnapshot<AppSettings> settings)
+        {
+            _settings = settings;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var testapiClient = _settings.Value.TestApiClent;
+            Uri.TryCreate(testapiClient, UriKind.RelativeOrAbsolute, out Uri uri1);
+
+            var ipEndPoint = GetHostIpEndPoint(uri1.DnsSafeHost, uri1.Port);
+            var url = $"http://{ipEndPoint.Address}:{ipEndPoint.Port}";
+            //var originUrl = $"{url}/api/values";
+            var originUrl = $"{testapiClient}/api/values";
+            using (var httpClient = new HttpClient())
+            {
+                var result = await httpClient.GetStringAsync(originUrl);
+                return Ok(result);
+            }
+        }
+
+        private IPEndPoint GetHostIpEndPoint(string hostName, string aspcoreUrl)
+        {
+            IPAddress[] ipaddress = Dns.GetHostAddresses(hostName);
+            var hostip = ipaddress.Where(ip => ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork).FirstOrDefault();
+            Uri.TryCreate(aspcoreUrl, UriKind.Absolute, out Uri uri);
+            return new IPEndPoint(hostip, uri.Port);
+        }
+
+
+        private IPEndPoint GetHostIpEndPoint(string hostName, int port)
+        {
+            IPAddress[] ipaddress = Dns.GetHostAddresses(hostName);
+            var hostip = ipaddress.Where(ip => ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork).FirstOrDefault();
+            return new IPEndPoint(hostip, port);
+        }
+
+    }
+}
---
+++ dockcompose/dockcomposeSimple/dockcomposeSimple/Startup.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace dockcomposeSimple
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<AppSettings>(Configuration);
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            Console.WriteLine($"TestEnv={Configuration["TestEnv"]}");
+            Console.WriteLine($"HostName={Configuration["HostName"]}");
+            Console.WriteLine($"ASPNETCORE_URLS={Configuration["ASPNETCORE_URLS"]}");
+
+            //获取docker下的ip地址
+            var hostName = Configuration["HostName"];
+            var aspurl = Configuration["ASPNETCORE_URLS"];
+            Uri.TryCreate(aspurl, UriKind.Absolute, out Uri uri);
+            var ipEndPoint = GetHostIpEndPoint(hostName, aspurl);
+
+            Console.WriteLine($"realIP={ipEndPoint.ToString()}");
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+        private IPEndPoint GetHostIpEndPoint(string hostName,string aspcoreUrl)
+        {
+            IPAddress[] ipaddress = Dns.GetHostAddresses(hostName);
+            var hostip = ipaddress.Where(ip => ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork).FirstOrDefault();
+            Uri.TryCreate(aspcoreUrl, UriKind.Absolute, out Uri uri);
+            return new IPEndPoint(hostip, uri.Port);
+        }
+    }
+}
